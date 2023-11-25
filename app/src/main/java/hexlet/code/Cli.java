4c92b93a7b59682238bc9f5de3911eada4aba598
vmanannikov <manhetan@gmail.com>
2023-11-26 01:34:11 +0300

    Исправлен Cli

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Cli {
     public static void greeting(Scanner sc) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
         var name = sc.next();
         System.out.println("Hello, " + name + "!");
     }
